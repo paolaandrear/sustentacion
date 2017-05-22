a8a692c4b439a0d1140769c05883b9e2b21a0cea
AnggyRuiz <anggydahianaruiz99@gmail.com>
2017-05-22 10:12:16 -0500

    Anggy

--- src/sustentacion/Sustentacion.java
+++ src/sustentacion/Sustentacion.java
@@ -18,27 +18,29 @@ public class Sustentacion {
      */
     public static void main(String[] args) {
         // 
-      int num1=0;
-      int num2=0;
-      int suma;
-        
-       num1=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese el primer numero "));
-        num2=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese el segundo numero  "));
-        
-        if(num1==num2){
-        
-        
-       
-  JOptionPane.showMessageDialog(null,"los numeros son iguales");
-  
-  
-  
-        }else{
-            JOptionPane.showMessageDialog(null,"los numeros son distintos");
+        int num1 = 0;
+        int num2 = 0;
+        int suma;
+        int respuesta = Integer.parseInt(JOptionPane.showInputDialog(null, "   Que Desea hacer \n1. saber Iguales  \n2 numero Mayor"));
+        if (respuesta == 1) {
+            num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese el primer numero "));
+            num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese el segundo numero  "));
+            if (num1 == num2) {
+                JOptionPane.showMessageDialog(null, "los numeros son iguales");
+            } else {
+                JOptionPane.showMessageDialog(null, "los numeros son distintos");
+            }
         }
-        
-    }
-    
-    }
-    
+        if (respuesta == 2) {
+            int nume1 = Integer.parseInt(JOptionPane.showInputDialog("ingrese el primer numero "));
+            int nume2 = Integer.parseInt(JOptionPane.showInputDialog("ingrese el segundo numero "));
+            if (nume1 > nume2) {
+                JOptionPane.showMessageDialog(null, "el numero mayor es: " + nume1);
+            } else if (nume2 > nume1) {
+                JOptionPane.showMessageDialog(null, "el numero mayor es: " + nume2);
+            }
 
+        }
+
+    }
+}
