d104752dbe97a6560896cc3dc5343a367b68bb89
paolaandrear <paoramos1999@gmail.com>
2017-05-22 09:44:17 -0500

    primera

---
+++ src/sustentacion/Sustentacion.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sustentacion;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author paolaramos
+ */
+public class Sustentacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+              int num1;
+      int num2;
+      int suma;
+        
+        int respuesta=Integer.parseInt(JOptionPane.showInputDialog(null,"Desea hacer una suma? \n1. si  \n2 no"));
+        if (respuesta==1) {
+           num1=Integer.parseInt(JOptionPane.showInputDialog("ingrese el primer numero "));
+           num2=Integer.parseInt(JOptionPane.showInputDialog("ingrese el segundo numero "));
+           suma = num1+num2;
+           JOptionPane.showMessageDialog(null,"el resultado de la suma es: "+suma);
+        }
+    }
+    
+    }
+    
+
