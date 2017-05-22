eaaf790faa8d6887f52d92b9407d9d813fd71341
paolaandrear <paoramos1999@gmail.com>
2017-05-22 09:59:36 -0500

    segunda

--- src/sustentacion/Sustentacion.java
+++ src/sustentacion/Sustentacion.java
@@ -18,17 +18,25 @@ public class Sustentacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-              int num1;
-      int num2;
+      int num1=0;
+      int num2=0;
       int suma;
         
-        int respuesta=Integer.parseInt(JOptionPane.showInputDialog(null,"Desea hacer una suma? \n1. si  \n2 no"));
-        if (respuesta==1) {
-           num1=Integer.parseInt(JOptionPane.showInputDialog("ingrese el primer numero "));
-           num2=Integer.parseInt(JOptionPane.showInputDialog("ingrese el segundo numero "));
-           suma = num1+num2;
-           JOptionPane.showMessageDialog(null,"el resultado de la suma es: "+suma);
+       num1=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese "));
+        num1=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese "));
+        
+        if(num1==num2){
+        
+        
+       
+  JOptionPane.showMessageDialog(null,"los numeros son iguales");
+  
+  
+  
+        }else{
+            JOptionPane.showMessageDialog(null,"los numeros son distintos");
         }
+        
     }
     
     }
